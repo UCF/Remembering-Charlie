28dcfca3b07516c640bc35a596fe4e9c9200d6c8
Jared Lang <jslang@mail.ucf.edu>
2010-12-01 11:03:30 -0500

    Removed more unused fields and altered text for comment box.

--- comments.php
+++ comments.php
@@ -51,7 +51,7 @@ if ( post_password_required() ) { ?>
 
     <div class="cancel-comment-reply"><?php cancel_comment_reply_link( 'Cancel Reply' ); ?></div>
     
-    <h3 id="leave-a-reply"><?php comment_form_title( 'Leave a Reply', 'Leave a Reply to %s' ); ?></h3> 
+    <h3 id="leave-a-reply"><?php comment_form_title( 'Leave a message', 'Leave a message to %s' ); ?></h3> 
     
     <?php if ( get_option( 'comment_registration' ) && !is_user_logged_in() ) : ?>
 	<p id="login-req" class="alert">You must be <a href="<?php echo get_option( 'siteurl' ); ?>/wp-login.php?redirect_to=<?php echo urlencode( get_permalink() ); ?>">logged in</a> to post a comment.</p>
@@ -70,20 +70,6 @@ if ( post_password_required() ) { ?>
             <label for="author"<?php if ( $req ) echo ' class="required"'; ?>>Name</label>
         <!--END #form-section-author-->
         </div>
-        
-        <!--BEGIN #form-section-email-->
-        <div id="form-section-email" class="form-section">
-            <input name="email" id="email" type="text" value="<?php echo $current_user->user_email; ?>" tabindex="2" <?php if ( $req ) echo "aria-required='true'"; ?> />
-            <label for="email"<?php if ( $req ) echo ' class="required"'; ?>>Email</label>
-        <!--END #form-section-email-->
-        </div>
-		
-        <!--BEGIN #form-section-url-->
-        <div id="form-section-url" class="form-section">
-            <input name="url" id="url" type="text" value="<?php echo $current_user->user_url; ?>" tabindex="3" />
-            <label for="url">Website</label>
-        <!--END #form-section-url-->
-        </div>
 		
 		<?php else : // If user isn't logged-in, ask them for their details ?>
         
@@ -94,26 +80,11 @@ if ( post_password_required() ) { ?>
         <!--END #form-section-author-->
         </div>
         
-        <!--BEGIN #form-section-email-->
-        <div id="form-section-email" class="form-section">
-            <input name="email" id="email" type="text" value="<?php echo $comment_author_email; ?>" tabindex="2" <?php if ( $req ) echo "aria-required='true'"; ?> />
-            <label for="email"<?php if ( $req ) echo ' class="required"'; ?>>Email</label>
-        <!--END #form-section-email-->
-        </div>
-		
-        <!--BEGIN #form-section-url-->
-        <div id="form-section-url" class="form-section">
-            <input name="url" id="url" type="text" value="<?php echo $comment_author_url; ?>" tabindex="3" />
-            <label for="url">Website</label>
-        <!--END #form-section-url-->
-        </div>
-        
 		<?php endif; // if ( is_user_logged_in() ) ?>
 		
 		<!--BEGIN #form-section-comment-->
         <div id="form-section-comment" class="form-section">
         	<textarea name="comment" id="comment" tabindex="4" rows="10" cols="65"></textarea>
-        	<p id="allowed-tags">You can use these <abbr title="HyperText Markup Language">HTML</abbr> tags and attributes: <span class="allowed-tags"><?php echo allowed_tags(); ?></span></p>
         <!--END #form-section-comment-->
         </div>
         
