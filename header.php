6c4e864e8b2ef727aa7155fe34bf3315b852f73e
Jared Lang <jslang@mail.ucf.edu>
2010-12-01 12:58:13 -0500

    Corrected incorrect date.

--- header.php
+++ header.php
@@ -8,7 +8,7 @@
 ?><!DOCTYPE HTML>
 <html <?php language_attributes(); ?>>
 <head>
-	<title>Remembering Dr. Millican (1906-2010)</title>
+	<title>Remembering Dr. Millican (1916-2010)</title>
 
 	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta name="description" content="<?php bloginfo( 'description' ) ?>" />
@@ -59,7 +59,7 @@
 		
 		<!-- Header -->
 		<div class="span-24 last" id="header">
-			<h1><a href="<?php bloginfo( 'url' ); ?>">Remembering Dr. Millican <span>(1906-2010)</span></a> <span id="first-pres">UCF&rsquo;s first president : 1965-1978</span></h1>
+			<h1><a href="<?php bloginfo( 'url' ); ?>">Remembering Dr. Millican <span>(1916-2010)</span></a> <span id="first-pres">UCF&rsquo;s first president : 1965-1978</span></h1>
 		</div><!-- /header -->
 		
 		<!--BEGIN content container -->
