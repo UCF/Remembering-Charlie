2062e203a0948d1ee42f08ed0566aa99d2b59983
Brandon Groves <brandon.groves@gmail.com>
2014-10-15 09:57:14 -0400

    removed flickr

--- page.php
+++ page.php
@@ -10,11 +10,11 @@ get_header();
 $comments_page = isset($_GET['comments']);
 ?>
 	<div class="span-24 last"><!-- col wrap -->
-		
+
 		<?php if(!$comments_page):?>
 		<div class="span-14">
 			<h2 class="entry-title"><?php the_title(); ?></h2>
-			
+
 			<!--BEGIN .hentry-->
 			<div id="post-<?php the_ID(); ?>" class="<?php semantic_entries(); ?>">
 	                  <?php if ( current_user_can( 'edit_post', $post->ID ) ): ?>
@@ -32,26 +32,19 @@ $comments_page = isset($_GET['comments']);
 				</div>
 			<!--END .hentry-->
 			</div>
-			
-			<?php if(function_exists('flickr_highslide')):?>
-			<div class="flickr">
-				<h2>Charles Millican Through the Years</h2>
-				<?php flickr_highslide(); ?>
-			</div>
-			<?php endif;?>
 		</div>
 		<?php else:?>
 		<div class="span-8">
 			<img src="<?php echo IMAGES . '/Dr-Millican-single.png'; ?>" alt="Dr. Millican">
 		</div>
 		<?php endif;?>
-		
+
 		<div class="comments <?php if($comments_page):?>span-14 prepend-1 append-1<?php else:?>prepend-1 span-9<?php endif;?> last">
 			<?php $comments_title = 'Remembering Charlie';?>
 			<h2 class="comments-title"><?=$comments_title?></h2>
 			<?php comments_template( '', true ); ?>
 		</div>
-		
+
 	</div><!-- /col wrap -->
 
 <?php get_footer(); ?>
